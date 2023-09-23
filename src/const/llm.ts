a205b244cd93f4548bdb89598dc30bb2fe3e7b25
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-23 06:17:53 -0600

    89

--- src/const/llm.ts
+++ src/const/llm.ts
@@ -7,6 +7,7 @@ export const LanguageModelWhiteList = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
+  'ft:gpt-3.5-turbo-0613:personal::7yezLDjd',
 ];
 
 export const DEFAULT_OPENAI_MODEL_LIST = [
@@ -14,4 +15,5 @@ export const DEFAULT_OPENAI_MODEL_LIST = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
+  'ft:gpt-3.5-turbo-0613:personal::7yezLDjd',
 ];
