2b50ae2464965caf32aaea815bbcc0b5feff3026
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-23 02:12:16 -0600

    Update Hero.tsx

--- src/pages/welcome/features/Banner/Hero.tsx
+++ src/pages/welcome/features/Banner/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = memo<{ width: number }>(({ width }) => {
     <>
       <LogoThree size={size.logo} style={{ marginTop: -size.logo / 5 }} />
       <div className={styles.title} style={{ fontSize: size.title }}>
-        <span style={mobile ? { fontSize: '1.2em' } : {}}>LobeChat</span>
+        <span style={mobile ? { fontSize: '1.2em' } : {}}>Sapiens🧬LABS</span>
         {mobile ? <br /> : ' '}
         {t('slogan.title')}
       </div>
