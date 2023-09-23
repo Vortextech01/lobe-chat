0669db6cc6a340f7c8a11b4fe3ccccd8a72045e5
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-23 03:56:44 -0600

    Update .i18nrc.js

--- .i18nrc.js
+++ .i18nrc.js
@@ -3,11 +3,11 @@ const { defineConfig } = require('@lobehub/i18n-cli');
 
 module.exports = defineConfig({
   reference: description,
-  entry: 'locales/zh_CN',
-  entryLocale: 'zh_CN',
+  entry: 'locales/en_US',
+  entryLocale: 'en_US',
   output: 'locales',
   outputLocales: ['zh_TW', 'en_US', 'ru_RU'],
   splitToken: 2500,
   temperature: 0,
-  modelName: 'gpt-3.5-turbo',
+  modelName: 'Basilisk-ai',
 });
