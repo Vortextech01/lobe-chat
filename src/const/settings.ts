88169406bc14461e7f95ec2cedc3f0dae1ef880d
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-23 10:53:38 -0600

    Update settings.ts

--- src/const/settings.ts
+++ src/const/settings.ts
@@ -12,7 +12,7 @@ import {
 export const DEFAULT_BASE_SETTINGS: GlobalBaseSettings = {
   avatar: '',
   fontSize: 14,
-  language: 'en_US',
+  language: 'zh-CN',
   neutralColor: '',
   password: '',
   primaryColor: '',
