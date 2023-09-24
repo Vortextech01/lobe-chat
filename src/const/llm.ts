4ba6f86f9b365df4847868397a3417a2b2502830
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-24 06:17:37 -0600

    Update llm.ts

--- src/const/llm.ts
+++ src/const/llm.ts
@@ -7,11 +7,13 @@ export const LanguageModelWhiteList = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
+  'ft:gpt-3.5-turbo-0613:personal::7yezLDjd'
 
 ];
 
 export const DEFAULT_OPENAI_MODEL_LIST = [
   'gpt-3.5-turbo',
-  'gpt-3.5-turbo-16k'
+  'gpt-3.5-turbo-16k',
+  'ft:gpt-3.5-turbo-0613:personal::7yezLDjd'
   
 ];
