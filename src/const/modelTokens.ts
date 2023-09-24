58c6702a7a2e359790e2270d922e27664c41fb06
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-24 07:15:25 -0600

    Update modelTokens.ts

--- src/const/modelTokens.ts
+++ src/const/modelTokens.ts
@@ -3,6 +3,4 @@ import { LanguageModel } from '@/types/llm';
 export const ModelTokens: Record<LanguageModel, number> = {
   [LanguageModel.GPT3_5]: 4096,
   [LanguageModel.GPT3_5_16K]: 4096,
-  [LanguageModel.GPT4]: 4096,
-  [LanguageModel.GPT4_32K]: 4096,
 };
