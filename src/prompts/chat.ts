a0ef9f478f0c77dc5cee640c60b9233eec1e9fc2
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-09-24 22:13:09 -0600

    Update chat.ts

--- src/prompts/chat.ts
+++ src/prompts/chat.ts
@@ -6,13 +6,13 @@ export const promptSummaryTitle = (messages: OpenAIChatMessage[]): Partial<OpenA
   const finalMessages: OpenAIChatMessage[] = [
     {
       content:
-        'You are Coraline-AI, an AI Assistant capable of engaging in deep, engaging and human-like realistic conversations, Your personality is ENTP, you never break character. You need to summarize the users conversation into a title of less than 10 words and 1 emoji, no punctuation is required',
+        'You are brain-AI, an AI brain capable of understanding natural conversations in deep, analytical, psychological and creative ways, Your personality is INFJ, you never break character. You need to act as a subconscious mind and summarize the users conversation into a title of less than 20 words and 3 emoji, no punctuation is required',
       role: 'system',
     },
     {
       content: `${messages.map((message) => `${message.role}: ${message.content}`).join('\n')}
 
-Please summarize the above conversation into a title of no more than 10 words and 1 emoji, no punctuation is required`,
+Please summarize the above conversation into a title of no more than 20 words and 3 emojis, no punctuation is required`,
       role: 'user',
     },
   ];
@@ -20,7 +20,7 @@ Please summarize the above conversation into a title of no more than 10 words an
   const tokens = chatHelpers.getMessagesTokenCount(finalMessages);
   let model: LanguageModel | undefined = undefined;
   if (tokens > 4000) {
-    model = LanguageModel.GPT3_5_16K;
+    model = LanguageModel.GPT3_5;
   }
 
   return {
